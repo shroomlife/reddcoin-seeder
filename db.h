dbb9e36c339521ba1378e9289f332c169baa77d3
laudney <ren@reddcoin.com>
2014-09-11 08:28:07 +0100

    Bump required versions.

--- db.h
+++ db.h
@@ -12,11 +12,11 @@
 
 #define MIN_RETRY 1000
 
-#define REQUIRE_VERSION 60000
+#define REQUIRE_VERSION 80000
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
-    return testnet ? 0 : 80000;
+    return testnet ? 0 : 300000;
 }
 
 std::string static inline ToString(const CService &ip) {
